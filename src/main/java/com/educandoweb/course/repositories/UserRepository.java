4906a357dff7d4c738065c577adc2239f950d588
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2024-10-28 15:01:46 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -7,5 +7,4 @@ import com.educandoweb.course.entities.User;
 // doesn't need to create the implementation of this interface, because the 
 // spring.data.jpa already does it
 public interface UserRepository extends JpaRepository<User, Long> {
-	
 }
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,10 +1,15 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.educandoweb.course.UserService;
 import com.educandoweb.course.entities.User;
 
 // this anotation indicates that this class is controlled by a rest controller
@@ -13,13 +18,24 @@ import com.educandoweb.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	// endpoint method to access users
 	// indicates that this method answers the get req from the http
 	@GetMapping
-	public ResponseEntity<User> findall() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<List<User>> findall() {
+		List<User> list = service.findAll();
 		// ok() returns success response
 		// body() returns the obj u in the body of the response
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	// the PathVariable annotation tells spring that the id comes from the url
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
