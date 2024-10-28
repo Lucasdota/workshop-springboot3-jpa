8733f614a237e728130c1169c18b10ab24ca76d3
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2024-10-28 20:12:39 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/enums/Product.java
+++ src/main/java/com/educandoweb/course/entities/enums/Product.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 import java.util.Set;
 
 import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,6 +40,9 @@ public class Product implements Serializable {
 	inverseJoinColumns = @JoinColumn(name= "category_id"))
 	// initiate variable so it doesn't initiate null, but empty
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		
@@ -92,6 +99,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
